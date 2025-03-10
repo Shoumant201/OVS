53f014dc562152f6376513559104c96b41bc4d89
Shoumant201 <np03cs4s240193@heraldcollege.edu.np>
2025-03-10 20:51:07 +0545

    Making layout and draft folders

---
+++ src/app/home/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from 'react'
+import { useState } from 'react';
+
+const page = () => {
+
+  const [count, setCount] = useState(0);
+
+    let counter = () =>{
+        setCount(count+1)
+    };
+
+  return (
+    <div className='w-screen h-screen flex justify-center items-center flex-col'>
+      <div className=''>
+        <p>{count}</p>
+      </div>
+      <button type="button" onClick={counter} className=' w-[100px] h-[30px] rounded-4xl bg-green-600 flex justify-center items-center'>
+        <p>Click ME!</p>
+      </button>
+    </div>
+  )
+}
+
+export default page
