421227a8743cb1c2478c33357c69eb057b68e06c
saransha10 <np03cs4s240206@heraldcollege.edu.np>
2025-03-28 20:51:12 +0545

    Fixed ban/unban functionality in admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,10 +2,6 @@ import express from 'express';
 import { authenticate, isAdminOrSuperAdmin } from '../middleware/authMiddleware.js';
 import { 
   addCommissioner, 
-  adminForgotPassword, 
-  adminLogin, 
-  adminRegister, 
-  adminResetPassword, 
   removeCommissioner 
 } from '../controllers/adminController.js';
 import { banUserController, unbanUserController } from '../controllers/userController.js';
@@ -15,10 +11,7 @@ const router = express.Router();
 router.post('/commissioner', authenticate, isAdminOrSuperAdmin, addCommissioner);
 router.delete('/commissioner/:id', authenticate, isAdminOrSuperAdmin, removeCommissioner);
 
-router.post('/adminRegister', adminRegister);
-router.post('/adminLogin', adminLogin);
-router.post('/adminForgot-password', adminForgotPassword);
-router.post('/adminReset-password', adminResetPassword);
+
 
 // Ban/Unban User Routes (Admin only)
 router.put('/users/:id/ban', authenticate, isAdminOrSuperAdmin, banUserController);
